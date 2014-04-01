422230dd19300f0ac8d32a41c4260e83ef514ea8
Adam Doupe <adamdoupe@gmail.com>
2014-04-01 12:42:11 -0700

    Moving to dot2tex for all the figures.

--- black-box/enemy-of-the-state/scripts/create_plots.sh
+++ black-box/enemy-of-the-state/scripts/create_plots.sh
@@ -5,8 +5,9 @@ do
 	file=$(basename $i)
 	just_name=${file%%.*}
 
-	gnuplot "data/$file"
-	epspdf "figures/$just_name.eps" "figures/$just_name.pdf"
+	echo "Not rebuilding the graphs, because only the original will work with gnuplot."
+	#gnuplot "data/$file"
+	#epspdf "figures/$just_name.eps" "figures/$just_name.pdf"
 done
 
 
@@ -17,6 +18,7 @@ for i in figures/*.dot;
 do
 	file=$(basename $i)
 	just_name=${file%%.*}
-	dot -Tps $i > figures/$just_name.ps && ps2pdf figures/$just_name.ps figures/$just_name.pdf
+#	dot -Tps $i > figures/$just_name.ps && ps2pdf figures/$just_name.ps figures/$just_name.pdf
+	python2 ./scripts/dot2tex-2.8.7/dot2tex/dot2tex.py -f tikz --figonly --autosize --usepdflatex --template "template.tex" --texmode raw figures/$just_name.dot > figures/$just_name.tex
 done
 
--- dedacota/dedacota/scripts/create_figures.sh
+++ dedacota/dedacota/scripts/create_figures.sh
@@ -17,6 +17,6 @@ for i in figures/*.dot;
 do
 	file=$(basename $i)
 	just_name=${file%%.*}
-	python2 ./scripts/dot2tex-2.8.7/dot2tex/dot2tex.py --figonly --autosize --usepdflatex --texmode raw figures/$just_name.dot > figures/$just_name.tex
+	python2 ./scripts/dot2tex-2.8.7/dot2tex/dot2tex.py -f tikz --figonly --autosize --usepdflatex --template "template.tex" --texmode raw figures/$just_name.dot > figures/$just_name.tex
 done
 
